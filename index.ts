d58f937af13ab3b0cd20112c425475aaba740cf8
Born-Bohrer <chem1000leaves@gmail.com>
2025-05-04 10:30:13 +0900

    error処理

--- index.ts
+++ index.ts
@@ -1,20 +1,25 @@
-import { fetchCotec } from "./src/fetch";
-import { cotecToJSON } from "./src/parsing";
+import { fetchCotec } from './src/fetch';
+import { cotecToJSON } from './src/parsing';
 import { writeFile } from 'node:fs';
 
 (async () => {
+  try {
     const ctc = await fetchCotec();
-
-    if (ctc instanceof Error) {
-        console.error(ctc);
-        return;
-    }
-    
     const parsed = await cotecToJSON(ctc);
 
-    writeFile('parsed-from-conlinguistics-wiki-list.ctc.json', JSON.stringify(parsed), (e) => {
-        if (e) console.error(e);
+    writeFile(
+      'parsed-from-conlinguistics-wiki-list.ctc.json',
+      JSON.stringify(parsed),
+      (e) => {
+        if (e) {
+            console.error(e);
+            process.exit(1);
+        }
         else console.log('writing to a file was successful');
-    });
+      }
+    );
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 })();
-
--- src/fetch.ts
+++ src/fetch.ts
@@ -3,7 +3,7 @@ export const fetchCotec = async () => {
     const url = 'https://kaeru2193.github.io/Conlang-List-Works/conlinguistics-wiki-list.ctc';
     const res = await fetch(url, { method: 'GET' });
     if (!res.ok) {
-        return Error(`failed to fetch: ${res.status}`);
+        throw Error(`failed to fetch: ${res.status} ${res.statusText}`);
     }
 
     return res.text();
