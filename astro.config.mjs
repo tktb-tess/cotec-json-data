cb3a434472b504014f71b3fcc941ae54488a6aa8
Born-Bohrer <chem1000leaves@gmail.com>
2025-09-17 22:32:55 +0900

    fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    site: 'https://tktb-tess.github.io',
     base: '/cotec-json-data',
     vite: {
         plugins: [tailwindcss()],
