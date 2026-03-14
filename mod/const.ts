c3e65bc6e6736103ab26e31a2909eb9fb76ba244
tktb-tess <chemusiclove6174@gmail.com>
2026-03-14 16:27:28 +0900

    aaa

--- mod/const.ts
+++ mod/const.ts
@@ -1,6 +1,5 @@
 const url =
   'https://kaeru2193.github.io/Conlang-List-Works/conlinguistics-wiki-list.ctc';
-
 const filePath = './public/out/conlinguistics-wiki-list-cotec.json';
 const filePathOld = './public/out/conlinguistics-wiki-list-cotec-old.json';
 const samplePath = './.arc/sample.ctc';
